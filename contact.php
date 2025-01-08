c9b5bf1efe08f84198e96017e1d7e1176eca3b1c
Davis Wollesen <daviswollesen@gmail.com>
2025-01-08 10:18:29 -0700

    ok

--- contact.php
+++ contact.php
@@ -1,69 +1,44 @@
 <?php
 
-//Retrieve form data. 
-//GET - user submitted data using AJAX
-//POST - in case user does not support javascript, we'll use POST instead
-$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
-$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
-$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+// Retrieve form data
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$comment = isset($_POST['comment']) ? $_POST['comment'] : '';
 
-//flag to indicate which method it uses. If POST set it to 1
+// Simple server-side validation for POST data
+$errors = [];
+if (!$name) $errors[] = 'Please enter your name.';
+if (!$email) $errors[] = 'Please enter your email.';
+if (!$comment) $errors[] = 'Please enter your message.';
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors[] = 'Please enter a valid email address.';
 
-if ($_POST) $post=1;
+// If the errors array is empty, send the mail
+if (empty($errors)) {
+    // Recipient - replace with your email
+    $to = 'daviswollesen@gmail.com';
+    // Sender - from the form
+    $from = $name . ' <' . $email . '>';
+    
+    // Subject and the HTML message
+    $subject = 'Message from ' . $name;
+    $message = 'Name: ' . $name . '<br/><br/>
+               Email: ' . $email . '<br/><br/>
+               Message: ' . nl2br($comment) . '<br/>';
 
-//Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
-
-//if the errors array is empty, send the mail
-if (!$errors) {
-
-	//recipient - replace your email here
-	$to = 'brookewollesen@gmail.com';	
-	//sender - from the form
-	$from = $name . ' <' . $email . '>';
-	
-	//subject and the html message
-	$subject = 'Message from ' . $name;	
-	$message = 'Name: ' . $name . '<br/><br/>
-		       Email: ' . $email . '<br/><br/>		
-		       Message: ' . nl2br($comment) . '<br/>';
-
-	//send the mail
-	$result = sendmail($to, $subject, $message, $from);
-	
-	//if POST was used, display the message straight away
-	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
-	//ajax script can react accordingly
-	//1 means success, 0 means failed
-	} else {
-		echo $result;	
-	}
-
-//if the errors array has values
+    // Send the mail
+    $result = sendmail($to, $subject, $message, $from);
+    
+    // Display the message
+    if ($result) {
+        echo 'Thank you! We have received your message.';
+    } else {
+        echo 'Sorry, unexpected error. Please try again later.';
+    }
 } else {
-	//display the errors message
-	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
-	echo '<a href="index.html">Back</a>';
-	exit;
+    // Display the errors message
+    foreach ($errors as $error) {
+        echo $error . '<br/>';
+    }
+    echo '<a href="index.html">Back</a>';
+    exit;
 }
-
-
-//Simple mail function with HTML header
-function sendmail($to, $subject, $message, $from) {
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-	$headers .= 'From: ' . $from . "\r\n";
-	
-	$result = mail($to,$subject,$message,$headers);
-	
-	if ($result) return 1;
-	else return 0;
-}
-
-?>
